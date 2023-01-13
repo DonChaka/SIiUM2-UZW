c24440e0d2550089a081d2cbb28ee5cb1eb4441d
DonChaka <44255641+DonChaka@users.noreply.github.com>
2023-01-14 00:31:27 +0100

    Added save and load functions

--- pacman-for-rl-main/pacman/Pacman.py
+++ pacman-for-rl-main/pacman/Pacman.py
@@ -1,9 +1,10 @@
 import random
 from abc import ABC, abstractmethod
 from .Position import Position, clamp
-from typing import Dict, Callable
-
+from typing import Dict, Callable, Optional
 import numpy as np
+from datetime import datetime
+from numpy import ndarray, loadtxt, savetxt
 from numpy.random import normal
 from .GameState import GameState
 from .Direction import Direction
@@ -60,9 +61,13 @@ class Pacman244827(Pacman):
     def __manhattanDistance(start: Position, other: Position) -> float:
         return abs(start.x - other.x) + abs(start.y - other.y)
 
+    @staticmethod
+    def __get_weights_from_file(fname: str) -> ndarray:
+        return loadtxt(fname=fname, delimiter=',')
+
     def __init__(
             self,
-            name: str = "Pacman 244827",
+            name: str = "Pacman_244827",
             n_weights: int = 1,
             action_space: tuple = tuple(Direction),
             features_function: Callable = None,
@@ -70,7 +75,8 @@ class Pacman244827(Pacman):
             epsilon: float = 1,
             eps_dec: float = 0.0001,
             eps_min: float = 0.01,
-            gamma: float = 0.9
+            gamma: float = 0.9,
+            weights_fname: Optional[str] = None
     ):
         """
         :param name: Name of the agent, returned with str() and repr()
@@ -82,6 +88,7 @@ class Pacman244827(Pacman):
         :param eps_dec: Value that will be subtracted from epsilon in every learning step. Default value = 0.0001
         :param eps_min: Minimal value for epsilon while training. Default value = 0.01
         :param gamma: Discount rate for Bellman equation. Default value = 0.9
+        :param weights_fname: Path to txt file containing weights. If provided, model weights will be loadewd from file, otherwise weigts will be pulled from normal distribution
         """
 
         self.name = name
@@ -99,13 +106,11 @@ class Pacman244827(Pacman):
         self.eps_dec = eps_dec
         self.eps_min = eps_min
         self.gamma = gamma
-        self.weights = normal(loc=0, scale=.5, size=self.n_features)
-        self.decision_cache = {
-            'state': None,
-            'action': None,
-            'reward': None,
-            '_state': None,
-        }
+        if weights_fname is None:
+            self.weights = normal(loc=0, scale=.5, size=self.n_features)
+        else:
+            self.weights = self.__get_weights_from_file(fname=weights_fname)
+        self.decision_cache = {}
         self.__n_wins = 0
         self.__n_loses = 0
 
@@ -115,7 +120,7 @@ class Pacman244827(Pacman):
     def __repr__(self):
         return self.name
 
-    def __feats_function(self, state: GameState, action: Direction):
+    def __feats_function(self, state: GameState, action: Direction) -> ndarray:
         def __map(value, leftMin, leftMax, rightMin, rightMax):
             leftSpan = leftMax - leftMin
             rightSpan = rightMax - rightMin
@@ -139,21 +144,24 @@ class Pacman244827(Pacman):
             feats.append(-1)
 
         # Distance to closest ghost that could hurt us
-        dists = [norm(self.__manhattanDistance(target, ghost['position'])) if not ghost['is_eatable'] else 0 for ghost in state.ghosts]
+        dists = [norm(self.__manhattanDistance(target, ghost['position'])) if not ghost['is_eatable'] else 0 for ghost
+                 in state.ghosts]
         if len(dists):
             feats.append((min(dists)))
         else:
             feats.append(0)
 
         # Distance to closest ghost that we can hurt
-        dists = [norm(self.__manhattanDistance(target, ghost['position'])) if ghost['is_eatable'] else 0 for ghost in state.ghosts]
+        dists = [norm(self.__manhattanDistance(target, ghost['position'])) if ghost['is_eatable'] else 0 for ghost in
+                 state.ghosts]
         if len(dists):
             feats.append((min(dists)))
         else:
             feats.append(0)
 
         # Distance to closest other pacman that could hurt us
-        dists = [norm(self.__manhattanDistance(target, other['position'])) if not other['is_eatable'] or not other['is_indestructible'] else x_size + y_size for other in state.other_pacmans]
+        dists = [norm(self.__manhattanDistance(target, other['position'])) if not other['is_eatable'] or not other[
+            'is_indestructible'] else x_size + y_size for other in state.other_pacmans]
         if len(dists):
             feats.append((min(dists)))
         else:
@@ -173,38 +181,36 @@ class Pacman244827(Pacman):
 
         return np.array(feats)
 
-    def q(self, state, action):
-        return np.dot(self.weights, self.features_function(state, action))
+    def __q(self, state: GameState, action: Direction) -> float:
+        return float(np.dot(self.weights, self.features_function(state, action)))
 
-    def update(self, state, action, reward, _state):
+    def __update(self, state: GameState, action: Direction, reward: int, _state: GameState) -> None:
         gamma = self.gamma
         lr = self.alpha
         if None in self.decision_cache.values():
-            return 0
+            return
 
-        delta = (reward + gamma * self.q(_state, self.get_best_action(_state))) - self.q(state, action)
+        delta = (reward + gamma * self.__q(_state, self.get_best_action(_state))) - self.__q(state, action)
         self.weights += lr * delta * self.features_function(state, action)
-        j = ((reward + gamma * self.q(_state, self.get_best_action(_state))) - self.q(state, action)) ** 2
 
         self.epsilon -= self.eps_dec
         self.epsilon = max(self.epsilon, self.eps_min)
-        return j
 
-    def get_best_action(self, state):
+    def get_best_action(self, state: GameState) -> Direction:
         possible_actions = self.action_space
 
-        scores = [self.q(state, action) for action in possible_actions]
+        scores = [self.__q(state, action) for action in possible_actions]
         bestScore = max(scores)
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
         chosenIndex = random.choice(bestIndices)
 
         return possible_actions[chosenIndex]
 
-    def make_move(self, state, invalid_move=False) -> Direction:
+    def make_move(self, state: GameState, invalid_move: bool = False) -> Direction:
         epsilon = self.epsilon
         self.decision_cache['_state'] = state
 
-        self.update(**self.decision_cache)
+        self.__update(**self.decision_cache)
 
         if random.random() < epsilon:
             chosen_action = random.choice(self.action_space)
@@ -216,22 +222,31 @@ class Pacman244827(Pacman):
 
         return chosen_action
 
-    def turn_off_learning(self):
+    def turn_off_learning(self) -> None:
         self.eps_min = 0
         self.epsilon = 0
         self.alpha = 0
 
-    def give_points(self, points):
+    def give_points(self, points: int) -> None:
         self.decision_cache['reward'] = points
 
-    def on_win(self, result: Dict["Pacman", int]):
+    def on_win(self, result: Dict["Pacman", int]) -> None:
         self.__n_wins += 1
 
-    def on_death(self):
+    def on_death(self) -> None:
         self.__n_loses += 1
 
-    def get_winrate(self):
-        return self.__n_wins / (self.__n_loses + self.__n_wins)
+    def get_winrate(self) -> float:
+        return self.__n_wins / (self.__n_loses + self.__n_wins) * 100 if self.__n_wins + self.__n_loses else 0.0
+
+    def reset_winrate(self) -> None:
+        self.__n_wins = 0
+        self.__n_loses = 0
+
+    def save(self) -> None:
+        dt_string = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+        fname = f'{self.name}_winrate={self.get_winrate():.2f}_{dt_string}.txt'
+        savetxt(fname=fname, delimiter=',')
 
 
 """
